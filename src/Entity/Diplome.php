d9707fdd376355f047cb51df30e7e0b096118d8e
coline85 <bodartcoline@gmail.com>
2023-03-30 09:17:43 +0200

    creation diplome

---
+++ src/Entity/Diplome.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DiplomeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DiplomeRepository::class)]
+class Diplome
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $diplome = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDiplome(): ?string
+    {
+        return $this->diplome;
+    }
+
+    public function setDiplome(string $diplome): self
+    {
+        $this->diplome = $diplome;
+
+        return $this;
+    }
+}
