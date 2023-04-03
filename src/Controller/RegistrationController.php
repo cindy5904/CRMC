8bd7fef469f3584ce95eb03ff8430d928cefc934
Romain Trochu <romain.trochu@gmail.com>
2023-04-03 16:45:08 +0200

    fix registration controller

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,23 +2,16 @@
 
 namespace App\Controller;
 
-use App\Entity\Company;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\AppAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
-use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
@@ -55,125 +48,5 @@ class RegistrationController extends AbstractController
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
-    }
-
-    #[Route('/cdd', name:'app_reg')]
-    public function registerCompany(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, UserAuthenticatorInterface $userAuthenticator, AppAuthenticator $authenticator)
-    {   
-        $user = new User();
-
-        $form = $this->createFormBuilder()
-            ->add('name', null, [
-                'label' => 'Nom',
-                'constraints' => [
-                    new Assert\NotBlank([
-                        'message' => 'Veuillez entrer un nom',
-                    ])
-                ],
-            ])
-            ->add('adress', null, [
-                'label' => 'Adresse',
-                'constraints' => [
-                    new Assert\NotBlank([
-                        'message' => 'Veuillez entrer une adresse',
-                    ])
-                ],
-            ])
-            ->add('city', null, [
-                'label' => 'Ville',
-                'constraints' => [
-                    new Assert\NotBlank([
-                        'message' => 'Veuillez entrer une ville',
-                    ])
-                ],
-            ])
-            ->add('postal_code', null, [
-                'label' => 'Code Postal',
-                'constraints' => [
-                    new Assert\NotBlank([
-                        'message' => 'Veuillez entrer un code postal',
-                    ])
-                ],
-            ])
-            ->add('email', EmailType::class, [
-                'label' => 'Email',
-                'constraints' => [
-                    new Assert\Email([
-                        'message' => 'Saisir un email valide'
-                    ])
-                ]
-            ])
-            ->add('siret', NumberType::class, [
-                'label' => 'Numéro de siret',
-                'constraints' => [
-                    new Assert\NotBlank([
-                        'message' => 'Saisie obligatoire'
-                    ]),
-                    new Assert\Length([
-                        'min' => 14,
-                        'minMessage' => ''
-                    ])
-                ]
-            ])
-            ->add('plainPassword', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new Assert\NotBlank([
-                        'message' => 'Veuillez entrer un mot de passe',
-                    ]),
-                    new Assert\Length([
-                        'min' => 6,
-                        'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
-                        'max' => 4096,
-                    ]),
-                ],
-                'first_options' => ['label' => 'Mot de passe'],
-                'second_options' => [
-                    'label' => 'Confirmer le mot de passe',
-                    'constraints' => [
-                        new Assert\NotBlank([
-                            'message' => 'Veuillez confirmer votre mot de passe',
-                        ])
-                    ],
-                ],
-            ])
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'label' => 'Accepter les conditions',
-                'constraints' => [
-                    new Assert\IsTrue([
-                        'message' => 'Vous devez accepter nos conditions.',
-                    ]),
-                ],
-            ])
-            ->getForm();
-            dump($form);
-
-            $form->handleRequest($request);
-            if ($form->isSubmitted() && $form->isValid()) {
-                $user->setPassword(
-                    $userPasswordHasher->hashPassword(
-                        $user,
-                        $form->get('plainPassword')->getData()
-                    )
-                );
-    
-                $user->setRoles(['ROLE_COMPANY']);
-                $user->setEmail('test@mail.com');
-    
-                $entityManager->persist($user);
-                $entityManager->flush();
-    
-                return $userAuthenticator->authenticateUser(
-                    $user,
-                    $authenticator,
-                    $request
-                );
-            }
-            return $this->render('registration/register-company.html.twig', [
-                'registrationFormCompany' => $form->createView()
-            ]);
     }
 }
