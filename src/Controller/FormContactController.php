9a64f7ed3e627a5f9245a685f1c25a316e8e44ce
Cindy <telcindy59@gmail.com>
2023-04-06 10:58:36 +0200

    creation du controller formcontact

---
+++ src/Controller/FormContactController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FormContactController extends AbstractController
+{
+    #[Route('/form/contact', name: 'app_form_contact')]
+    public function index(): Response
+    {
+        return $this->render('form_contact/index.html.twig', [
+            'controller_name' => 'FormContactController',
+        ]);
+    }
+}
