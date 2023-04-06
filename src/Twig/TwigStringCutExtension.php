7823e953fcbe648780cafd8745f4f8224fb2ca9b
Romain Trochu <romain.trochu@gmail.com>
2023-04-06 14:03:26 +0200

    create filter twig to cut content

---
+++ src/Twig/TwigStringCutExtension.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class TwigStringCutExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [new TwigFilter('cut', [$this, 'cutFilter'])];
+    }
+
+    public function cutFilter($content): string
+    {
+        $newContent = substr($content, 0, 100);
+        $limit = strrpos($newContent, " ");
+        $content = substr($content, 1, $limit);
+
+        return $content;
+    }
+}
+
