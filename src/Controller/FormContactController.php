a1a3a0ebb303e0011fc9bf8eff49ed9860e65b8e
Cindy <telcindy59@gmail.com>
2023-04-06 14:24:04 +0200

    integration du form contact

--- src/Controller/FormContactController.php
+++ src/Controller/FormContactController.php
@@ -3,16 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FormContactController extends AbstractController
 {
-    #[Route('/form/contact', name: 'app_form_contact')]
-    public function index(): Response
+    #[Route('/contact', name: 'app_form_contact')]
+    public function index(Request $request): Response
     {
-        return $this->render('form_contact/index.html.twig', [
-            'controller_name' => 'FormContactController',
-        ]);
+        if ($request->isMethod('POST')) {
+           
+            
+            
+            $this->addFlash('success', 'Votre message a bien été envoyé!');
+            
+            return $this->redirectToRoute('app_form_contact');
+        }
+        return $this->render('form_contact/index.html.twig');
     }
+
 }
