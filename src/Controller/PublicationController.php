023a69007b69f855df32f47b027b2aa99153edde
Romain Trochu <romain.trochu@gmail.com>
2023-04-08 08:34:49 +0200

    try to order publcation if  role_user  (display publication of company & formation)

--- src/Controller/PublicationController.php
+++ src/Controller/PublicationController.php
@@ -15,13 +15,32 @@ class PublicationController extends AbstractController
     #[Route('/publication', name: 'app_publication')]
     public function index(PublicationRepository $publicationRepository, UserRepository $userRepository): Response
     {   
+        /** @var User */
         $user = $this->getUser();
         $word = 'A modifier';
         $verify = $userRepository->verify($user, $word);
         $users = $userRepository->findAll();
-        
+        // récupération de tous les utilisateurs
+        $publi = [];
+        // initialise un tableau vide de publications
+        foreach($users as $user){
+            $roles = $user->getRoles();
+            // récupération du role des utilisateurs
+            if($roles[0] === 'ROLE_COMPANY' ||$roles[0] === 'ROLE_FORMATION'){
+                //savoir si l'utilisateur récupéré est une entreprise ou centre de formation
+                $value = $user->getId();
+                // récupération de l'id de l'utilisateur entreprise ou centre de formation
+                $publications = $publicationRepository->findBy(['publicationUser' => $value]);
+                // récupération des publications (si plusieurs publication, on récupère un tableau de publication)
+                foreach($publications as $publication){
+                    // on récupère chaque publication
+                    $publi[] = $publication;
+                    // on injecte chaque publication dans le tableau des publications qu'on va passer à la vue
+                } ;
+            };
+        };
         return $this->render('publication/index.html.twig', [
-            'posts' => $publicationRepository->findBy([], ['createdAt' => 'DESC']),
+            'posts' => $publi,
             'users' => $users,
             'verify' => $verify,
         ]);
