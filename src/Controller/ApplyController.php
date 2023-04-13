ce1a682abe26927b94d553aa35ff4929200f3a9a
Cindy <telcindy59@gmail.com>
2023-04-13 20:09:48 +0200

    upload

--- src/Controller/ApplyController.php
+++ src/Controller/ApplyController.php
@@ -2,17 +2,82 @@
 
 namespace App\Controller;
 
+use App\Entity\Apply;
+use App\Repository\PublicationRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\File;
 
 class ApplyController extends AbstractController
 {
     #[Route('/apply', name: 'app_apply')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, PublicationRepository $publication ): Response
     {
-        return $this->render('apply/index.html.twig', [
-            'controller_name' => 'ApplyController',
+        /** @var User */
+        $apply = new Apply();
+        $user = $this->getUser();
+        
+
+        $form = $this->createFormBuilder()
+        
+            ->add('upload', FileType::class, array
+            ('data_class' => null), [
+                'mapped' => 'false',
+                'required' => 'false',
+                'constraints' => [
+                    new File ([
+                        'mimeTypes' => [
+                            'cv.pdf',
+                            'cv.word',
+                        ]
+                    ])
+                ]
+            ]
+            )
+            ->add('publication_id', HiddenType::class)
+            ->add('message', TextareaType::class, [
+                'label' => 'Entrez Votre message',
+                ]
+            )
+            ->getForm();
+
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $apply->setCreatedAt(new \DateTimeImmutable());
+                $apply->setUser($user);
+                $posterFile = $form->get('upload')->getData();
+                $publicationId = $form['publication_id']->getData();
+                
+
+                
+                if ($posterFile) {
+                    $fileName = uniqid().'.'.$posterFile->guessExtension(); 
+                    $posterFile->move($this->getParameter('cv_upload'), $fileName);
+                    
+                   $apply->setUpload($fileName);
+                }
+                
+                $apply->setApplyPublication($publication);
+                $apply->setMessage($form['message']->getData());
+                
+                $applyPublication = $apply->getApplyPublication();
+
+
+
+                $entityManager->persist($apply);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app_publication');
+            }
+        return $this->render('apply/index.html.twig',[
+            'form' => $form,
         ]);
     }
 }
