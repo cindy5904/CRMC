03a85d0b184b877dcfe77c272546b7923632e2e9
Mélanie <djoey59@live.fr>
2023-05-08 14:17:08 +0200

    ad

--- src/Controller/ApplyController.php
+++ src/Controller/ApplyController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints\File;
 
+
 class ApplyController extends AbstractController
 {
     #[Route('ROLE_USER')]
